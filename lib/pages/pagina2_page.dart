c50c7fe402fb9e0bf5a879b17d351e397b75f9f3
Carlos Barcos <fbarcos8@hotmail.com>
2021-03-27 23:10:08 -0500

    Inicio

---
+++ lib/pages/pagina2_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Pagina2Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagina 2'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              child: Text('Establecer Usuario',style: TextStyle(color: Colors.white)),
+              color: Colors.blue,
+              onPressed: (){},
+            ),
+            MaterialButton(
+              child: Text('Cambiar edad',style: TextStyle(color: Colors.white)),
+              color: Colors.blue,
+              onPressed: (){},
+            ),
+            MaterialButton(
+              child: Text('Añadir Profesion',style: TextStyle(color: Colors.white)),
+              color: Colors.blue,
+              onPressed: (){},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
